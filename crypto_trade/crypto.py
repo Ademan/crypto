ef1a62ee694b8d41741f86d17e7c51f8f6f6b45d
Daniel Roberts <Ademan555@gmail.com>
2013-11-23 02:48:23 -0800

    Moved examples out of main file. Renamed main class.

--- crypto_trade/crypto.py
+++ crypto_trade/crypto.py
@@ -10,7 +10,7 @@ import urllib2
 import json
 import httplib
 
-class crypto:
+class CryptoTrade(object):
 	timeout = 15
 	tryout = 8
 
@@ -87,34 +87,3 @@ class crypto:
 			tries += 1
 			if time.time() - t0 > self.timeout or tries > self.tryout:
 				raise Exception('Timeout')
-				
-
-#Example configuration				
-#key='MYKEY'
-#secret='MYSECRET'
-#cryp = crypto(key,secret)				
-
-#			
-#Example placing an order			
-#cryp.req('trade',{"pair":"ltc_btc","type":"Buy","amount":ltc_bid_amount,"rate":price_to_bid})		
-#						
-#Example Cancelling an order
-#cryp.req('cancelorder',{"orderid":order_id})
-#
-#Example Get Funds
-#account_info=cryp.req('getinfo')
-#orig_crypto_ltc_amount=float(account_info['data']['funds']['ltc'])
-#orig_crypto_btc_amount=float(account_info['data']['funds']['btc'])
-#orig_crypto_usd_amount=float(account_info['data']['funds']['usd'])
-#
-#Example Get Last Two Days of Orders
-#unix_time_lag=int(time.time()-166400)
-#for order in cryp.req('ordershistory',{'start_date':unix_time_lag})['data']:
-#    if  order["status"]=="Active" or order["status"]=='Partly Completed':
-#	cryp.req('cancelorder',{"orderid":int(order["id"])})
-#
-#Example Market Depth
-#cryp.reqpublic('depth/ltc_btc')
-
-
-	
