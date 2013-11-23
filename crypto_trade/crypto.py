f4e99c396328f01bcabcf31902451a9cc211b645
Daniel Roberts <Ademan555@gmail.com>
2013-11-23 02:56:59 -0800

    Pulled throttling code out of the API. It is a separate concern.

--- crypto_trade/crypto.py
+++ crypto_trade/crypto.py
@@ -10,29 +10,30 @@ import urllib2
 import json
 import httplib
 
-class CryptoTrade(object):
-	timeout = 15
-	tryout = 8
+class Throttle(object):
+    def __init__(self, window, max):
+        self.window = window
+        self.max = max
+        self.count = 0
+        self.last = time.time() - window # We don't want to wait even if we immediately call throttle
 
+    def peek(self):
+        diff = time.time() - self.last
+
+    def throttle(self):
+        diff = time.time() - self.last
+        if diff > self.window:
+                self.count = 0
+                self.last = time.time()
+        self.count += 1
+        if self.count > self.max:
+            time.sleep(self.window - diff)
+
+class CryptoTrade(object):
 	def __init__(self, key='', secret='', agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0'):
 		self.key, self.secret, self.agent = key, secret, agent
-		self.time = {'init': time.time(), 'req': time.time()}
-		self.reqs = {'max': 10, 'window': 10, 'curr': 0}
 		self.baseprivate = 'https://www.crypto-trade.com/api/1/private/'	# for authenticated POST requests
 		self.basepublic = 'https://www.crypto-trade.com/api/1/'				# for unauthenticated GET requests
-
-	def throttle(self):
-		# check that in a given time window (10 seconds),
-		# no more than a maximum number of requests (10)
-		# have been sent, otherwise sleep for a bit
-		diff = time.time() - self.time['req']
-		if diff > self.reqs['window']:
-			self.reqs['curr'] = 0
-			self.time['req'] = time.time()
-		self.reqs['curr'] += 1
-		if self.reqs['curr'] > self.reqs['max']:
-			print 'Request limit reached...'
-			time.sleep(self.reqs['window'] - diff)
 
 	def reqpublic(self, path):
 		# Send a request to the public API
@@ -61,29 +62,14 @@ class CryptoTrade(object):
 		})
 
 	def req(self, path, inp={}):
-		t0 = time.time()
-		tries = 0
-		while True:
-			# check if have been making too many requests
-			self.throttle()
+                # send request
+                inp['nonce'] = str(int(time.time() * 1e3))
+                inpstr = urllib.urlencode(inp.items())
+                req = self.makereq(path, inpstr)
+                response = urllib2.urlopen(req, inpstr)
 
-			try:
-				# send request
-				inp['nonce'] = str(int(time.time() * 1e3))
-				inpstr = urllib.urlencode(inp.items())
-				req = self.makereq(path, inpstr)
-				response = urllib2.urlopen(req, inpstr)
-
-				# interpret json response
-				output = json.load(response)
-				if 'error' in output:
-					raise ValueError(output['error'])
-				return output
-				
-			except Exception as e:
-				print "Error: %s" % e
-
-			# don't wait too long
-			tries += 1
-			if time.time() - t0 > self.timeout or tries > self.tryout:
-				raise Exception('Timeout')
+                # interpret json response
+                output = json.load(response)
+                if 'error' in output:
+                        raise ValueError(output['error'])
+                return output
